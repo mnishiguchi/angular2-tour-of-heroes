90f65adfc1d7634ccc8cfae3f83de01e5d646425
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-01-26 12:45:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# angular2-tour-of-heroes
+- https://angular.io/docs/ts/latest/tutorial/toh-pt1.html
---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from 'angular2/core';
+// import { RouteConfig, ROUTER_DIRECTIVES } from 'angular2/router';
+
+// Defines the AppComponent, which is the root component.
+
+@Component({
+  selector: 'app',
+  template: '<h1>Boku-no hajimete-no Angular 2 App</h1>'
+})
+// @RouteConfig([
+
+// ])
+export class AppComponent {}
